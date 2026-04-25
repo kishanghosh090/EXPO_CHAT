5dcd7724a824e05ace770ee63d7993d00ed35c6c
kishanghosh090 <kishanghosh090@gmail.com>
2026-04-25 19:39:41 +0530

    initiate

---
+++ backend/dist/src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const PORT = 4005;
+app.get("/", (req, res) => {
+    return res.status(200).json({ msg: "hello from chaiii code" });
+});
+app.listen(PORT, () => {
+    console.log(`Server is listing at PORT ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
