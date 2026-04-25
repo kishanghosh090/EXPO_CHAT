c03b9b47b7965dd91f2b1e29f0bfdb4ad3ddfebe
kishanghosh090 <kishanghosh090@gmail.com>
2026-04-25 21:24:30 +0530

    add better-auth

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -1,6 +1,10 @@
+import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import { auth } from "./lib/auth";
 const app = express();
 const PORT = 4005;
+app.all("/api/auth/*", toNodeHandler(auth));
+app.use(express.json());
 app.get("/", (req, res) => {
     return res.status(200).json({ msg: "hello from chaiii code" });
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
+import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import { auth } from "./lib/auth";
 
 const app = express();
 const PORT = 4005;
 
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
+app.use(express.json());
 app.get("/", (req, res) => {
   return res.status(200).json({ msg: "hello from chaiii code" });
 });
